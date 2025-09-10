396a8fd61c9a80b3731159eedde2c878e32f228b
Jiming Yang <179237@cloud.rangitoto.school.nz>
2025-09-11 10:30:30 +1200

    made the gambling game and started the maths game (I changed it) (it doesnt work yet)

--- Python/hello.py
+++ Python/hello.py
@@ -1,5 +1,41 @@
-root = Tk(screenName=None, baseName=None, className='Tk', useTk=1)
-import tkinter
-m = tkinter.Tk()
-m.mainloop()
+import tkinter as tk
+import random
+from tkinter import *
+r = tk.Tk()
+r.title('Home Page')
+button = tk.Button(r, text='Stop', width=25, command=r.destroy)
+button.pack()
+r.mainloop()
+
+#gambling game
+score = 0.0
+
+
+gamble = float(input("How much would you like to gamble?"))
+thing = random.choice(["1", "2"])
+if thing == "1":
+    score = score + gamble
+    print(f"WINNER! you won! you have {score} dollars")
+if thing == "2":
+    score = score - gamble
+    print(f"LOSER! you lost {score} dollars")
+    
+#game 2: maths quiz
+stop = "stop"
+while True:
+    try:
+        no1 = random.randint(1, 20)
+        no2 = random.randint(1, 20)
+        useranswer = (input(f"What is {no1} + {no2}? "))
+        actual = (int(no1) + int(no2))
+        if useranswer == "stop":
+            break
+        int(useranswer)
+        if useranswer == actual:
+            print("Correct!")
+            score = score + 1
+        if useranswer != actual: 
+            print("Incorrect! try again")
+    except ValueError:
+        print("Please enter a number or 'stop' to end the game")
 
